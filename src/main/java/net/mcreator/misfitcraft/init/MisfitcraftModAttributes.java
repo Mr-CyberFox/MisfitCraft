4712e722e36325dc3ebd3ab06d42907c70277632
jason <jason@Computer-Jason>
2026-07-12 19:58:04 +0200

    MCreator Merge commit 'af2773e629dd1f75d882c6e2186deb91799c543b' into e682a4b863325adf2fd983c8d56e830cfd4f1cc5

--- src/main/java/net/mcreator/misfitcraft/init/MisfitcraftModAttributes.java
+++ src/main/java/net/mcreator/misfitcraft/init/MisfitcraftModAttributes.java
@@ -20,10 +20,11 @@ import net.mcreator.misfitcraft.MisfitcraftMod;
 public class MisfitcraftModAttributes {
 	public static final DeferredRegister<Attribute> REGISTRY = DeferredRegister.create(BuiltInRegistries.ATTRIBUTE, MisfitcraftMod.MODID);
 	public static final DeferredHolder<Attribute, Attribute> MAGIC_POWER = REGISTRY.register("magic_power", () -> new RangedAttribute("attribute.misfitcraft.magic_power", 100, 0, 1000000000).setSyncable(true));
-	public static final DeferredHolder<Attribute, Attribute> SOURCE_POINTS = REGISTRY.register("source_points", () -> new RangedAttribute("attribute.misfitcraft.source_points", 100, 0, 100).setSyncable(true));
+	public static final DeferredHolder<Attribute, Attribute> SOURCE_POINTS = REGISTRY.register("source_points", () -> new RangedAttribute("attribute.misfitcraft.source_points", 20, 0, 5000).setSyncable(true));
 	public static final DeferredHolder<Attribute, Attribute> MANA_TIMER = REGISTRY.register("mana_timer", () -> new RangedAttribute("attribute.misfitcraft.mana_timer", 0, 0, 20).setSyncable(true));
 	public static final DeferredHolder<Attribute, Attribute> MAX_MAGIC_POWER = REGISTRY.register("max_magic_power", () -> new RangedAttribute("attribute.misfitcraft.max_magic_power", 100, 0, 1000000000).setSyncable(true));
 	public static final DeferredHolder<Attribute, Attribute> RACE = REGISTRY.register("race", () -> new RangedAttribute("attribute.misfitcraft.race", 0, 0, 4).setSyncable(true));
+	public static final DeferredHolder<Attribute, Attribute> MAX_SOURCE_POINTS = REGISTRY.register("max_source_points", () -> new RangedAttribute("attribute.misfitcraft.max_source_points", 20, 0, 10000).setSyncable(true));
 
 	@SubscribeEvent
 	public static void addAttributes(EntityAttributeModificationEvent event) {
@@ -32,5 +33,6 @@ public class MisfitcraftModAttributes {
 		event.add(EntityType.PLAYER, MANA_TIMER);
 		event.getTypes().forEach(entity -> event.add(entity, MAX_MAGIC_POWER));
 		event.add(EntityType.PLAYER, RACE);
+		event.getTypes().forEach(entity -> event.add(entity, MAX_SOURCE_POINTS));
 	}
 }
